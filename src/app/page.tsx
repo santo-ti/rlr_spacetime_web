9af5d2fa91c0031603d8a7cfcab8756365de9523
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2024-08-26 17:46:56 -0300

    feat: Alter className order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/img/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
-        <div className="blur-full absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50" />
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/* Stripes */}
-        <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
         {/* Sign In */}
         <a
@@ -43,7 +43,7 @@ export default function Home() {
           </div>
 
           <a
-            className="font-alt inline-block rounded-full bg-green-500 px-5 py-3 text-sm uppercase leading-none text-black transition-colors hover:bg-green-700"
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-700"
             href=""
           >
             CADASTRAR LEMBRANçA
